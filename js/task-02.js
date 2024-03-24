c6eceaee651a2d376db84da070da656e127a8121
Piotr Frontczak <piotrf2023@gmail.com>
2024-03-24 13:43:12 +0100

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,15 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ingredientsList = document.getElementById('ingredients');
+
+ingredients.forEach(ingredient => {
+  const li = document.createElement('li');
+  
+  li.classList.add('item');
+  
+  li.textContent = ingredient;
+  
+  ingredientsList.appendChild(li);
+});
