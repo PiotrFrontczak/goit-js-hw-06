cd5e2ee12c4c5c0c454f1a0f962c4108903e84f9
Piotr Frontczak <piotrf2023@gmail.com>
2024-03-25 14:02:08 +0100

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,24 @@
+document.addEventListener('DOMContentLoaded', function () {
+  const loginForm = document.querySelector('.login-form');
 
+  loginForm.addEventListener('submit', function (event) {
+    event.preventDefault();
+
+    const emailInput = loginForm.elements.email;
+    const passwordInput = loginForm.elements.password;
+
+    if (emailInput.value.trim() === '' || passwordInput.value.trim() === '') {
+      alert('Wszystkie pola muszą być wypełnione!');
+      return; 
+    }
+
+    const formObj = {
+      email: emailInput.value,
+      password: passwordInput.value
+    };
+
+    console.log(formObj);
+
+    loginForm.reset();
+  });
+});
