8eee75c7122bb0b58cbade75bbd4a4a2a8480817
Piotr Frontczak <piotrf2023@gmail.com>
2024-03-25 14:20:52 +0100

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,14 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+document.addEventListener('DOMContentLoaded', function() {
+  const colorSpan = document.querySelector('.color');
+  const changeColorButton = document.querySelector('.change-color');
+
+  changeColorButton.addEventListener('click', function() {
+    const randomColor = getRandomHexColor();
+    document.body.style.backgroundColor = randomColor;
+    colorSpan.textContent = randomColor;
+  });
+});
