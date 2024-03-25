3ed669a9e76c180da5b6b060d466548f2f1bf688
Piotr Frontczak <piotrf2023@gmail.com>
2024-03-25 11:04:39 +0100

    Update task-04.js

---
+++ js/task-04.js
@@ -0,0 +1,24 @@
+let counterValue = 0;
+
+const decrementButton = document.querySelector('[data-action="decrement"]');
+const incrementButton = document.querySelector('[data-action="increment"]');
+const counterValueElem = document.getElementById('value');
+
+function updateCounterValue() {
+  counterValueElem.textContent = counterValue;
+}
+
+function decrement() {
+  counterValue--;
+  updateCounterValue();
+}
+
+function increment() {
+  counterValue++;
+  updateCounterValue();
+}
+
+decrementButton.addEventListener('click', decrement);
+incrementButton.addEventListener('click', increment);
+
+updateCounterValue();
